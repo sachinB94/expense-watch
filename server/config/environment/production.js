e28293c19ab5062088040bff843e89ae3bb97646
Sachin Bansal <sachin@sifium.co>
2016-04-17 16:28:18 +0530

    Host set to  0.0.0.0

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   port: process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 9000,
-  ip: process.env.OPENSHIFT_NODEJS_IP || process.env.IP || '127.0.0.1',
+  ip: process.env.OPENSHIFT_NODEJS_IP || process.env.IP || '0.0.0.0',
   mongo: {
     uri: 'mongodb://expense-watch:expense-watch@ds011281.mlab.com:11281/expense-watch'
   }
